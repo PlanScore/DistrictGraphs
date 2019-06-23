4fe0ae3c5d9e5113d6750dc9acead5c49904b0a8
Michal Migurski <mike@teczno.com>
2019-06-22 17:56:29 -0700

    Added polygonize.polygonize_assignment() to DistrictGraphs-read_file function

--- DistrictGraphs/read_file.py
+++ DistrictGraphs/read_file.py
@@ -1,5 +1,8 @@
+import csv, io, os
+import boto3
+import networkx
 import itsdangerous
-from . import constants
+from . import constants, polygonize
 
 def lambda_handler(event, context):
     '''
@@ -7,10 +10,21 @@ def lambda_handler(event, context):
     signature = event['queryStringParameters']['signature']
     id = itsdangerous.Signer(constants.SECRET).unsign(signature).decode('utf8')
     
+    s3 = boto3.client('s3', endpoint_url=constants.S3_ENDPOINT_URL)
+    object = s3.get_object(Bucket='districtgraphs', Key=id)
+    rows = csv.reader(io.StringIO(object['Body'].read().decode('utf8')))
+    assignments = list(rows)
+    
+    path = os.path.join(os.path.dirname(__file__), 'tests/data/madison3.pickle')
+    graph = networkx.read_gpickle(path)
+    districts = polygonize.polygonize_assignment(assignments, graph)
+    
     return {
         'statusCode': '200',
         'headers': {'Access-Control-Allow-Origin': '*'},
         'body': {
             'id': id,
+            'assignments': assignments,
+            'districts': {id: str(geom) for (id, geom) in districts.items()},
             }
         }
